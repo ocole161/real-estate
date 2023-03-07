c8fae704e10ae70bc9c342b73ca3a1c07132165e
Owen <ocole161@gmail.com>
2023-03-07 09:43:18 -0700

    react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import Auth from './components/Auth';
+import CreateAccount from './components/CreateAccount';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Auth />
+      <Routes>
+        <Route path="auth" element={ <Auth /> } />
+        <Route path="signup" element={<CreateAccount />} />
+      </Routes>
     </div>
   );
 }
